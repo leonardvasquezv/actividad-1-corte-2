66a9268ac326651964950a935017442c65349670
Leonard Vasquez <leonardvasquez22@gmail.com>
2023-09-22 00:07:06 -0500

    Correcciones

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const path = require('path'); // Agregamos el módulo 'path' para manipular rutas de archivos
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,21 +11,41 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.post('/formulario', (req, res) => {
-  const nombre = req.body.nombre;
-  const correo = req.body.correo;
+  const usuario_id = req.body.usuario_id;
+  const usuario_nombre = req.body.usuario_nombre;
+  const usuario_apellido = req.body.usuario_apellido;
+  const libro_titulo = req.body.libro_titulo;
+  const libro_autor = req.body.libro_autor;
+  const libro_editorial = req.body.libro_editorial;
+  const libro_anio = req.body.libro_anio;
 
-  const datos = { nombre, correo };
-  const datosJSON = JSON.stringify(datos);
+  if (!usuario_id || !usuario_nombre || !usuario_apellido || !libro_titulo || !libro_autor || !libro_editorial || !libro_anio) {
+    res.redirect('/error.html');
+  } else {
+    const datos = {
+      usuario_id,
+      usuario_nombre,
+      usuario_apellido,
+      libro_titulo,
+      libro_autor,
+      libro_editorial,
+      libro_anio
+    };
 
-  fs.writeFile('datos.txt', datosJSON, (err) => {
-    if (err) {
-      console.error('Hubo un error:', err);
-      res.status(500).send('Error al guardar los datos.');
-    } else {
-      console.log('Datos guardados correctamente.');
-      res.status(200).send('Datos guardados correctamente.');
-    }
-  });
+    const datosJSON = JSON.stringify(datos);
+
+    const rutaArchivo = path.join(__dirname, 'data', `${usuario_id}.txt`);
+
+    fs.writeFile(rutaArchivo, datosJSON, (err) => {
+      if (err) {
+        console.error('Hubo un error:', err);
+        res.status(500).send('Error al guardar los datos.');
+      } else {
+        console.log('Datos guardados correctamente.');
+        res.status(200).send('Datos guardados correctamente.');
+      }
+    });
+  }
 });
 
 app.listen(port, () => {
